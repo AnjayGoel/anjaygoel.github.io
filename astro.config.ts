8f29ebf2534ed28249b19ee717df4a5fed71edb9
anjay.g@dashtoon.com <anjay.goel@gmail.com>
2024-09-29 16:21:38 +0530

    WIP

--- astro.config.ts
+++ astro.config.ts
@@ -58,7 +58,7 @@ export default defineConfig({
 			// short_name: "Astro_Cactus",
 			description: siteConfig.description,
 			lang: siteConfig.lang,
-			icon: "public/icon.svg", // the source for generating favicon & icons
+			icon: "public/icon.png", // the source for generating favicon & icons
 			icons: [
 				{
 					src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
