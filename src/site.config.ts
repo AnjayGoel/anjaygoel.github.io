463148c981980d17b4ca0f2493f23c3c3009202d
anjay.g@dashtoon.com <anjay.goel@gmail.com>
2024-10-02 14:37:39 +0530

    Fix description

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,8 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "Personal Blog",
+	description:
+		"I write about software development, technology and occasionally other stuff like economics & finance.",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
